0de892fbd261de89b97cd61f982d1e8e115f91b7
richardxtpg <64601727+richardxtpg@users.noreply.github.com>
2020-05-03 18:06:24 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,3 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -13,11 +12,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+    await alive.edit("**xTPG A.I is online.** \n`Battery Status : ` **69%**\n\n"
+                     f"`My Smartest Owner`: {DEFAULTUSER}\n\n"
+                     "`Telethon version:` **6.9.0**\n`Python:` **3.7.3**\n"
+                     "`Bot Community :` **xTPG**\n\n`I can do this all day.\n`"
+                     "**Bot Creator:** @thePredatorGuy\n\n"
+                     "**Following’s not really my style.**\n\n"
+                     
 
